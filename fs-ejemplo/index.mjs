d3171be7e601d58090f0eb503feeb2053b553c5c
Adalberto-JAB <laboral.jab@gmail.com>
2025-02-06 22:15:04 -0300

    Add carpeta fs-ejemplo

---
+++ fs-ejemplo/index.mjs
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+// Lee un archivo de manera asincrona
+fs.readFile('./data/example.txt', 'utf-8', (err, data) => {
+  if ( err ) throw err;
+  console.log('Contenido del archivo:', data);
+});
+
+// Escribir en un nuevo archivo
+fs.writeFile('./data/newFile.txt', 'Contenido nuevo', (err) => {
+  if ( err ) throw err;
+  console.log('Archivo creado y escrito');
+});
+
+// Renombrar un archivo
+fs.rename('./data/newFile.txt', './data/renamedFile.txt', (err) => {
+  if ( err ) throw err;
+  console.log('Archivo Renombrado');
+});
+
+
+
